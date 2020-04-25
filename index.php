fe44c7de656bad6e92daba064fa3df3d858aaa0d
Olexandr Panov <futupas@gmail.com>
2020-04-26 00:56:01 +0300

    try to get from db

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
     echo "index.php";
     $servername = getenv('db_server');
-$username = getenv('db_username');
-$password = getenv('db_password');
-$dbname = getenv('db_dbname');
+    $username = getenv('db_username');
+    $password = getenv('db_password');
+    $dbname = getenv('db_dbname');
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    echo "index.php";
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+echo 'Connected successfully';
+
+
+$sql = 'SELECT `id`, `step` FROM `users`;';
+$result = $conn->query($sql);
+
+// echo '  '.json_encode($result).'  ';
+// echo json_encode($result);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo 'id: ' . $row['id']. ' - key: ' . $row['step'].'<br>';
+    }
+} else {
+    echo '0 results';
+}
+$conn->close();
 ?>
