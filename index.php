b7b1c7cb8536d03260d32e93af9b97e64a9c3f15
Olexandr Panov <futupas@gmail.com>
2020-04-26 00:50:38 +0300

    try to connect to db

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
     echo "index.php";
+    $servername = getenv('db_server');
+$username = getenv('db_username');
+$password = getenv('db_password');
+$dbname = getenv('db_dbname');
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    echo "index.php";
 ?>
